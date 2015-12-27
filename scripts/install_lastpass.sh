b94eb823c0b2e61120bcaf7a592772e1fa39679f
Christopher Burr <chrisburr73@googlemail.com>
2015-12-27 19:42:43 +0000

    Fix missing lastpass dependency

--- scripts/install_lastpass.sh
+++ scripts/install_lastpass.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
-pacaur --noconfirm --noedit -S lastpass-cli
+# pinentry requires libgtk-x11-2.0.so.0 which is provided by gtk2
+pacaur --noconfirm --noedit -S lastpass-cli xclip gtk2
 lpass login $(echo Y2hyaXNidXJyNzNAZ29vZ2xlbWFpbC5jb20K | base64 --decode)
