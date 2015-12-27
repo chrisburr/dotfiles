9b03819d997ea1703e6cf3f0334983afe07fbe42
Christopher Burr <chrisburr73@googlemail.com>
2015-12-27 21:31:16 +0000

    Copy krb5.conf to /etc/ when installing afs

--- scripts/install_openafs.sh
+++ scripts/install_openafs.sh
@@ -2,6 +2,10 @@
 
 pacaur --noconfirm --noedit -S openafs openafs-modules-dkms linux-headers dkms
 sudo echo "cern.ch" | sudo tee /etc/openafs/ThisCell
+cp ../assets/krb5.conf .
+sudo chown --reference=/etc/krb5.conf krb5.conf
+sudo chmod --reference=/etc/krb5.conf krb5.conf
+sudo mv krb5.conf /etc/krb5.conf
 sudo systemctl enable dkms
 sudo systemctl start dkms
 sudo systemctl enable openafs-client
