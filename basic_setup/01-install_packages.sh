2fcb1feca543d762949e5e3516453640e9eee85c
Christopher Burr <chrisburr73@googlemail.com>
2015-12-29 21:30:29 +0000

    Always install autossh

--- basic_setup/01-install_packages.sh
+++ basic_setup/01-install_packages.sh
@@ -3,7 +3,7 @@
 sudo pacman --noconfirm -Syy
 sudo pacman --noconfirm -Syu
 
-sudo pacman --noconfirm -S base-devel zsh git expect wget tmux svn mosh parted
+sudo pacman --noconfirm -S base-devel zsh git expect wget tmux svn mosh parted autossh
 
 sudo localectl set-locale LANG=en_GB.UTF-8
 sudo sed -i 's/#en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/g' /etc/locale.gen
